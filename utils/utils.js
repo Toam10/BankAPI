ef8bd2d6ec9876acaaa5c2a17881a70efe3fa8fb
Toam10 <78689759+Toam10@users.noreply.github.com>
2022-01-17 00:24:02 +0200

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,18 +1,21 @@
 const fs = require("fs");
 
-const parserClients = (path) => {
-  return JSON.parse(fs.readFileSync(path, "utf-8"));
-};
-// const addClient = (usersData, path) => {
-//   fs.writeFileSync(path, JSON.stringify(usersData));
-// }
+const parserClients = (path) => JSON.parse(fs.readFileSync(path, "utf-8"));
+// should be called parserUsers
+// use in one line to return this value
+
+
 const updateDataBase = (usersData, path) => {
   fs.writeFileSync(path, JSON.stringify(usersData));
 }
+// this function isnt return anything that why i dont tell you to make is one line that is clearer
 
 
 module.exports = {parserClients, updateDataBase}
 
 //module.exports = {parserClients, addClient, deleteClient};
+
+
+// use need to add here alot more function because most of the easy functionaly is inside the controllers you can do it better
 
 
